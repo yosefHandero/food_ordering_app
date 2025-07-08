084e968cf3e23c0d442514ec842387a5ebb53555
yosef <yosefpetrosh@gmail.com>
2025-07-08 08:54:32 -0500

    initial set up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {SplashScreen, Stack} from "expo-router";
+import "./global.css"
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+ export default function RootLayout() {
+   const [fontsLoaded, error] = useFonts({
+     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+     "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+     "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+     "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+     "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+   });
+   useEffect(() => {
+     if (error) throw error;
+     if(fontsLoaded) SplashScreen.hideAsync();
+   }, [fontsLoaded, error]);
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+   return <Stack screenOptions={{headerShown:false}}/>
 }
---
+++ components/CartButton.tsx
@@ -0,0 +1,20 @@
+import {View, Text, TouchableOpacity, Image} from 'react-native'
+import {images} from "@/constants";
+
+const CartButton = () => {
+    const totalItems = 10
+
+    return (
+        <TouchableOpacity className="cart-btn" onPress={()=> {}}>
+            <Image source={images.bag} className="size-5" resizeMode="contain" />
+
+            {totalItems > 0 && (
+                <View className="cart-badge">
+                    <Text className="small-bold text-white">{totalItems}</Text>
+                </View>
+            )}
+
+        </TouchableOpacity>
+    )
+}
+export default CartButton
